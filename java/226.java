acecc3b4202a8c74ab3f1020526386a70322eaf9
trajepl <trajepl@gmail.com>
2017-03-25 11:13:54 +0800

    226 tree

---
+++ java/226.java
@@ -0,0 +1,47 @@
+/**
+
+ * Definition for a binary tree node.
+
+ * public class TreeNode {
+
+ *     int val;
+
+ *     TreeNode left;
+
+ *     TreeNode right;
+
+ *     TreeNode(int x) { val = x; }
+
+ * }
+
+ */
+
+public class Solution {
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root == null || (root.left == null && root.right == null)) return root;
+
+        
+
+        TreeNode tmp = null;
+
+        tmp = root.left;
+
+        root.left = root.right;
+
+        root.right = tmp;
+
+        
+
+        root.left = invertTree(root.left);
+
+        root.right = invertTree(root.right);
+
+        
+
+        return root;
+
+    }
+
+}
