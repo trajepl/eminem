9b23b79469e022c2db37ffe25ade9834428a461a
trajepl <trajepl@gmail.com>
2017-04-12 11:01:02 +0800

    404 tree

--- java/404.java
+++ java/404.java
@@ -22,3 +22,34 @@ public class Solution {
         return dfs_left(root.left, true) + dfs_left(root.right, false);
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if(root == null) return 0;
+        int ret = 0;
+        Stack<TreeNode> left_s = new Stack<>();
+        left_s.push(root);
+        
+        while(!left_s.empty()) {
+            TreeNode node = left_s.pop();
+            if(node.left != null) {
+                if(node.left.left == null && node.left.right == null) ret += node.left.val;
+                else left_s.push(node.left);
+            }
+            if(node.right != null) {
+                if(node.right.left != null || node.right.right != null) left_s.push(node.right);
+            }
+        
+        }
+      return ret;
+    }
+}
