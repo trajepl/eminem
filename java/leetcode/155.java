a7b8bff7442b803ae7ebb62d60a9a2e496e64eb6
trajepl <trajepl@gmail.com>
2017-07-02 12:08:08 +0800

    155 Min Stack:stack

--- java/leetcode/155.java
+++ java/leetcode/155.java
@@ -1,38 +1,68 @@
-public class MinStack {
-    Stack<Integer> minStack;
-    Queue<Integer> minHeap;
-    int min;
-    /** initialize your data structure here. */
-    public MinStack() {
-        minStack = new Stack<>();
-        min = Integer.MAX_VALUE;
-        minHeap = new PriorityQueue<>();
-    }
-    
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
     public void push(int x) {
-        minHeap.offer(x);
-        minStack.push(x);
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
     }
-    
+
     public void pop() {
-        minHeap.poll();
-        minStack.pop();
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
     }
-    
+
     public int top() {
-        return minStack.peek();
+        return stack.peek();
     }
-    
+
     public int getMin() {
-        return minHeap.peek();
+        return min;
     }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
+public class MinStack {
+    long min;
+    Stack<Long> stack;
+
+    public MinStack(){
+        stack=new Stack<>();
+    }
+
+    public void push(int x) {
+        if (stack.isEmpty()){
+            stack.push(0L);
+            min=x;
+        }else{
+            stack.push(x-min);//Could be negative if min value needs to change
+            if (x<min) min=x;
+        }
+    }
+
+    public void pop() {
+        if (stack.isEmpty()) return;
+
+        long pop=stack.pop();
+
+        if (pop<0)  min=min-pop;//If negative, increase the min value
+
+    }
+
+    public int top() {
+        long top=stack.peek();
+        if (top>0){
+            return (int)(top+min);
+        }else{
+           return (int)(min);
+        }
+    }
+
+    public int getMin() {
+        return (int)min;
+    }
+}
