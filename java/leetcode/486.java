cb94bf368c81b465be93f4e602bdaf756c456cd2
trajep <trajepl@gmail.com>
2017-09-29 14:12:18 +0800

    486 Predict the Winner:dp minimax

---
+++ java/leetcode/486.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean PredictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) { dp[i][i] = nums[i]; }
+        for (int len = 1; len < n; len++) {
+            for (int i = 0; i < n - len; i++) {
+                int j = i + len;
+                dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] >= 0;
+    }
+    
+    
+    public boolean PredictTheWinner(int[] nums) {
+        if (nums == null) { return true; }
+        int n = nums.length;
+        if ((n & 1) == 0) { return true; } 
+        int[] dp = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (i == j) {
+                    dp[i] = nums[i];
+                } else {
+                    dp[j] = Math.max(nums[i] - dp[j], nums[j] - dp[j - 1]);
+                }
+            }
+        }
+        return dp[n - 1] >= 0;
+    }
+
+}
+
