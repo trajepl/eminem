5f964ffa08c893804a8678800eac0bfe1bd86729
trajep <trajepl@gmail.com>
2017-10-22 21:11:35 +0800

    072 Edit Distance:dp string O(mn) space & O(n) space

--- java/leetcode/072.java
+++ java/leetcode/072.java
@@ -17,3 +17,26 @@ class Solution {
         return dp[m][n];
     }
 }
+
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+
+        int[] cur = new int[m+1];
+        for(int i = 1; i <= m; i++) {
+            cur[i] = i;
+        }
+        for (int j = 1; j <= n; j++) {
+            int pre = cur[0];
+            cur[0] = j;
+            for(int i = 1; i <= m; i++) {
+                int tmp = cur[i];
+                if(word1.charAt[i-1] == word2.charAt[j-1]) cur[i] = pre;
+                else 
+                    cur[i] = Math.min(pre+1, Math.min(cur[i] + 1, cur[i-1] + 1));
+                pre = tmp;
+            }
+        }
+        return cur[m];
+    }
+}
