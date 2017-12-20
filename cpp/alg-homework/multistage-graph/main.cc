7803183e0b3d159802faf50e5aa45b7a21598dc1
trajep <trajepl@gmail.com>
2017-12-20 11:18:42 +0800

    multistage-graph

--- cpp/alg-homework/multistage-graph/main.cc
+++ cpp/alg-homework/multistage-graph/main.cc
@@ -5,15 +5,15 @@ using namespace std;
 
 #define MAX_VAL ((unsigned)(-1) >> 1)
 
-int multistage_graph(vector<vector<int>> &graph, vector<vector<int>> &dist) {
+int multistage_graph(vector<vector<int>> &graph, vector<vector<int>> &d) {
     vector<vector<int>> dp(1, vector<int>(graph[0].size(), 0));
-    for (int k = 1; k < graph.size(); k++) {
+    for(int k = 1; k < graph.size(); k++) {
         dp.emplace_back(graph[k].size(), MAX_VAL);
-        for (int i = 0; i < graph[k].size(); i++) {
-            for (int j = 0; j < graph[k-1].size(); j++) {
+        for(int i = 0; i < graph[k].size(); i++) {
+            for(int j = 0; j < graph[k-1].size(); j++) {
                 int s = graph[k-1][j], t = graph[k][i];
-                if (dist[s][t] == -1) continue;
-                dp[k][i] = min(dp[k][i], dp[k-1][j] + dist[s][t]);
+                if(d[s][t] == MAX_VAL) continue;
+                dp[k][i] = min(dp[k][i], dp[k-1][j] + d[s][t]);
             }
         }
     }
@@ -25,7 +25,7 @@ int main() {
     vector<vector<int>> graph, d;
 
     n = 12;
-    d = vector<vector<int>>(n+1, vector<int>(n+1, -1));
+    d = vector<vector<int>>(n+1, vector<int>(n+1, MAX_VAL));
     graph = {{1}, {2, 3, 4, 5}, {6, 7, 8}, {9, 10, 11}, {12}};
     d[1][2] = 9; d[1][3] = 7; d[1][4] = 2; d[1][5] = 2;
     d[2][6] = 4; d[2][7] = 2; d[2][8] = 2;
