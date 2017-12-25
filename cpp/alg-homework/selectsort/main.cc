a6576048e23768d65c092b4844cbc8aa06dfcc83
trajep <trajepl@gmail.com>
2017-12-25 15:25:56 +0800

    selectsort add test

--- cpp/alg-homework/selectsort/main.cc
+++ cpp/alg-homework/selectsort/main.cc
@@ -64,17 +64,29 @@ void print(int *arr, int len) {
     printf("\n");
 }
 
-int main() {
-    int arr[] = {19, 17, 1, 5, 14, 6, 0, 198, 235};
-    int len = sizeof(arr) / sizeof(int);
+void test(int *arr, int len) {
 
-    printf("original array: ");
+    printf("orginal array: ");
     print(arr, len);
 
     for(int i = 0; i < len; i++) {
         int ret = select2(arr, 0, len, i+1);
         printf("No.%d number: %d\n", ret, arr[ret]);
     }
+}
+
+int main() {
+    int arr[] = {1, 5, 2, 10, 9, 16, 5, 8};
+    int len = sizeof(arr) / sizeof(int);
+    test(arr, len);
+
+    int arr1[] = {5, 2, 10, 9, 16, 5, 8};
+    len = sizeof(arr1) / sizeof(int);
+    test(arr1, len);
+
+    int arr2[] = {9, 6, 5, 8};
+    len = sizeof(arr2) / sizeof(int);
+    test(arr2,len);
 
     return 0;
 }
